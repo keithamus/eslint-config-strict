50ca2a70f44d4b24a438d45920264dc2d0f53895
Keith Cirkel <github@keithcirkel.co.uk>
2015-06-24 16:55:44 +0100

    Fix up es5 lint rules

--- es5.js
+++ es5.js
@@ -1,4 +1,10 @@
 module.exports = require('./defaults');
 
+// enforce the spacing around the * in generator functions
+module.exports.rules['generator-star-spacing'] = 0;
+// require let or const instead of var
 module.exports.rules['no-var'] = 0;
+// require method and property shorthand syntax for object literals
+module.exports.rules['object-shorthand'] = 0;
+// suggest using of const declaration for variables that are never modified after declared
 module.exports.rules['prefer-const'] = 0;
