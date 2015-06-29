020927f00aee7ae233dbcfc6040bbac258bf25e1
Keith Cirkel <github@keithcirkel.co.uk>
2015-06-29 14:16:47 +0100

    Add mocha environment

---
+++ mocha.js
@@ -0,0 +1,9 @@
+module.exports = require('./defaults');
+
+module.exports.env.mocha = true;
+
+// Common assertion library globals
+module.exports.globals.assert = false;
+module.exports.globals.should = false;
+module.exports.globals.must = false;
+module.exports.globals.expect = false;
