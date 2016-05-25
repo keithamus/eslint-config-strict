eaf57c185f672922678b3874fb85ee588f305862
Evan Hensleigh <futuraprime@gmail.com>
2016-05-25 14:32:15 +0100

    feat: Allowing d3 in variable names

--- d3.js
+++ d3.js
@@ -3,12 +3,22 @@ var mainConfig = require('./es5');
 var idLengthOptions = mainConfig.rules['id-length'][1];
 module.exports = {
   rules : {
-    'id-length' : [
+    'id-length': [
       2,
       {
         'min': idLengthOptions.min,
         'max': idLengthOptions.max,
-        'exceptions': idLengthOptions.exceptions.concat([ 'd', 'dx', 'dy', 'r', 'cx', 'cy' ]),
+        'exceptions': idLengthOptions.exceptions.concat([ 'd', 'dx', 'dy', 'r', 'cx', 'cy', 'd3' ]),
+      },
+    ],
+    'id-match': [
+      2,
+      // identifiers must always be camel cased, unless they are all caps
+      // they may not include numbers, except as a part of the phrase "d3"
+      '^([A-Za-z]*([dD]3)?[A-Za-z]*|[A-Z][A-Z_]*((D3)?[A-Z_]*[A-Z]|D3))$',
+      {
+        // identifiers in properties are also checked
+        'properties': true,
       },
     ],
   },
