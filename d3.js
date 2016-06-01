6139d9a8628241a6f807f7e35910b0e062e2e532
Evan Hensleigh <futuraprime@gmail.com>
2016-06-01 09:40:52 +0100

    feat: Add d3 lint options
    This adds a `strict/d3` subset which can be added on top of the already `strict` options - the d3 subset allows for specific d3 identifiers, such as `d3` and `d` - common identifiers in d3 code.

---
+++ d3.js
@@ -0,0 +1,25 @@
+'use strict';
+var mainConfig = require('./es5');
+var idLengthOptions = mainConfig.rules['id-length'][1];
+var idMatchOptions = mainConfig.rules['id-match'][2];
+module.exports = {
+  rules : {
+    'id-length': [
+      2,
+      {
+        'min': idLengthOptions.min,
+        'max': idLengthOptions.max,
+        'exceptions': idLengthOptions.exceptions.concat([ 'd', 'dx', 'dy', 'd3' ]),
+      },
+    ],
+    'id-match': [
+      2,
+      // identifiers must always be camel cased, unless they are all caps
+      // they may not include numbers, except as a part of the phrase "d3"
+      '^([A-Za-z]*([dD]3)?[A-Za-z]*|([A-Z]|(D3))[A-Z_]*((D3)?[A-Z_]*[A-Z]|D3))$',
+      {
+        'properties': idMatchOptions.properties,
+      },
+    ],
+  },
+};
